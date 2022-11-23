7ef37a6513db95dcae774712fb6626ef5569c3fb
Mukul <ramdevmukul800@gmail.com>
2022-11-23 15:56:30 +0530

    in progress

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import './App.css';
 import Counter from './components/Counter';
 import Context from './components/Context';
 import Bat from './components/Bat';
-
+import {Provider} from 'react-redux'
+import store from './redux/store';
 function App() {
   const [theme,setTheme]=useState(false)
   return (
-    <Context.Provider value={theme} >
-      <button onClick={()=>setTheme(!theme)}>Change Theme</button>
-      <h3>For setting theme I've used context api</h3>
+   <div>
+    {/* <Context.Provider value={theme} >
+       <button onClick={()=>setTheme(!theme)}>Change Theme</button>
+       <h3>For setting theme I've used context api</h3>
 
       
-        <Counter/>
-        <Bat/>
+         <Counter/>
+         <Bat/>
         
       
-    </Context.Provider>
+     </Context.Provider> */}
+
+    <Provider store={store}>
+      <h2>Done with redux</h2>
+
+<Bat/>
+    </Provider>
+    </div>
   );
 }
 
--- src/components/Bat.js
+++ src/components/Bat.js
@@ -1,13 +1,28 @@
 import React,{useState} from 'react'
+import { connect } from 'react-redux'
 
-const Bat = () => {
-    const[bat,setBat]=useState(20)
+const Bat = (props) => {
+    // const[bat,setBat]=useState(20)
+
+    console.log(props)
   return (
     <div>
-        <h1>Bats:{bat}</h1>
-        <button onClick={()=>setBat(bat-1)}>Buy Bat</button>
+        <h1>Bats:{props.bats}</h1>
+    <button onClick={props.buyBat}>Buy Bat</button>
+        
     </div>
   )
 }
+const mapStateToProps=(state)=>{
+    return{
+        bats: state.bats
+    }
+}
+const mapDispatchToProps=(dispatch)=>{
+    return{
+        buyBat:()=>
+        dispatch({type:'BUY_BAT'})
+    }
+}
 
-export default Bat
+export default connect(mapStateToProps,mapDispatchToProps)(Bat)
